1277637226add2dcbb21b6fdc8ef3a183fafa89d
Alexander Ebadirad <alex@ebadirad.com>
2016-04-29 00:18:34 -0700

    Minor cleanup of demo, it's starting to come together, still a bit ugly

--- src/demo/js/main.js
+++ src/demo/js/main.js
@@ -1,31 +1,39 @@
-function buildFormData(){
-	var data = {
-		name: $('#name').val(),
-		fuzzy: $('#fuzzy').val(),
-		admin1Code: $('#admin1Code').val(),
-		admin2Code: $('#admin2Code').val(),
-		countryCode: $('#countryCode').val(),
-		population: $('#population').val(),
-		featureClass: $('#featuerClass').val(),
-		featureCode: $('#featureCode').val(),
-		limit: $('#limit').val(),
-		geo: window.geo
-	};
-	return data;
+function buildFormData() {
+
+  var population;
+  if ($('#population-amount')) {
+    population = {
+      amount: $('#population-amount').val(),
+      inequality: $('#population-inequality').val()
+    };
+  }
+  var data = {
+    name: $('#name').val(),
+    fuzzy: $('#fuzzy').val(),
+    admin1Code: $('#admin1Code').val(),
+    admin2Code: $('#admin2Code').val(),
+    countryCode: $('#countryCode').val(),
+    population: population,
+    featureClass: $('#featuerClass').val(),
+    featureCode: $('#featureCode').val(),
+    limit: $('#limit').val(),
+    geo: window.geo
+  };
+  return data;
 };
 
 
-function getCountries(){
-	var jqxhr = $.get( "example.php", function() {
-  alert( "success" );
-})
-  .done(function() {
-    alert( "second success" );
-  })
-  .fail(function() {
-    alert( "error" );
-  })
-  .always(function() {
-    alert( "finished" );
-  });
+function getCountries() {
+  var jqxhr = $.get("example.php", function() {
+      alert("success");
+    })
+    .done(function() {
+      alert("second success");
+    })
+    .fail(function() {
+      alert("error");
+    })
+    .always(function() {
+      alert("finished");
+    });
 }
